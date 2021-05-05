185c76eb3d05ff416373c2192028f0db1e172437
Your Name <you@example.com>
2021-05-05 13:42:24 +0700

    An

---
+++ WatchStore25/WatchStore25/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WatchStore25.Models;
+
+namespace WatchStore25.Controllers
+{
+
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult TaiKhoan()
+        {   
+            return View();
+        } 
+    }
+}
