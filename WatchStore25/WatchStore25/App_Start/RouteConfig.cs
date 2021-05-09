fab5a5c77f9cc66da95937b3605ed0e773254b35
trinhphu9872 <trinhphu9872@gmail.com>
2021-05-09 17:26:32 +0700

    Upcode ViewAdmin 09/05/2021

--- WatchStore25/WatchStore25/App_Start/RouteConfig.cs
+++ WatchStore25/WatchStore25/App_Start/RouteConfig.cs
@@ -18,6 +18,31 @@ namespace WatchStore25
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            routes.MapRoute(
+                         name: "AdHome",
+                         url: "{controller}/{action}",
+                         defaults: new { controller = "Admin", action = "HomeAdmin" }
+                     );
+            routes.MapRoute(
+                         name: "AdCus",
+                         url: "{controller}/{action}",
+                         defaults: new { controller = "Admin", action = "CustomerManager" }
+                     );
+            routes.MapRoute(
+                         name: "AdOrder",
+                         url: "{controller}/{action}",
+                         defaults: new { controller = "Admin", action = "OrderManager" }
+                     );
+            routes.MapRoute(
+                         name: "AdProduct",
+                         url: "{controller}/{action}",
+                         defaults: new { controller = "Admin", action = "ProductManager" }
+                     );
+            routes.MapRoute(
+                           name: "AdAddProduct",
+                           url: "{controller}/{action}",
+                           defaults: new { controller = "Admin", action = "AddNewProductManager" }
+                       );
         }
     }
 }
---
+++ WatchStore25/WatchStore25/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WatchStore25.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult HomeAdmin()
+        {
+            return View();
+        }
+        public ActionResult CustomerManager()
+        {
+            return View();
+        }
+        public ActionResult OrderManager()
+        {
+            return View();
+        }
+        public ActionResult ProductManager()
+        {
+            return View();
+        }
+        public ActionResult AddNewProductManager()
+        {
+            return View();
+        }
+
+
+    }
+}
